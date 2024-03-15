d45373fa674aba1297ba62807c192bddb766654a
Marcos Sales <m4rcos.sales@gmail.com>
2024-03-15 12:20:54 -0300

    diretivas segunda parte

--- src/app/components/template/footer/footer.component.ts
+++ src/app/components/template/footer/footer.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { BlueDirective } from '../../../directives/blue.directive';
 
-
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatToolbar, BlueDirective],
+  imports: [MatToolbar, BlueDirective, ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
--- src/app/views/production/production.component.ts
+++ src/app/views/production/production.component.ts
@@ -1,12 +1,14 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-production',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './production.component.html',
   styleUrl: './production.component.css'
 })
 export class ProductionComponent {
-
+  listaNumeros  = [1,2,3]
 }
